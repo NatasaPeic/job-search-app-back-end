ccc48a3a96ad8d00fc30dcfb8a01dc4016aa1ce8
NatasaPeic <peic.natasa@gmail.com>
2016-08-21 10:12:16 -0400

    Add client_origin

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module JobSearchAppBackEnd
     # Cross-Origin Resource Sharing
     config.middleware.use Rack::Cors do
       allow do
-        origins ENV['CLIENT_URL'] || 'http://localhost:8080'
+        origins ENV['CLIENT_ORIGIN'] || 'https://natasapeic.github.io/job-search-app-front-end'
         resource '*',
                  headers: :any,
                  methods: [:options, :get,
